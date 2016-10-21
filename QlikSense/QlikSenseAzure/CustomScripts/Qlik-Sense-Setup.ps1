e2fe5e9f7d2803754f97d210b7566af2a528192b
Kristofer Liljeblad <kristol@microsoft.com>
2016-10-21 12:08:31 +0200

    Added functionality to read passed variables

--- QlikSense/QlikSenseAzure/CustomScripts/Qlik-Sense-Setup.ps1
+++ QlikSense/QlikSenseAzure/CustomScripts/Qlik-Sense-Setup.ps1
@@ -2,14 +2,21 @@
 
 $tmpfile = "$env:TEMP\qliktest.txt"
 
+# Reading environment variables
 "Environment test" | Out-File $tmpfile -Append
 [Environment]::UserName | Out-File $tmpfile -Append
 [Environment]::UserDomainName | Out-File $tmpfile -Append
 [Environment]::MachineName | Out-File $tmpfile -Append
 
+# Reading parameters passed to the script file
 "Parameter test" | Out-File $tmpfile -Append
+$vmName = $Args[0]
+$userName = $Args[1]
 
+$vmName | Out-File $tmpfile -Append
+$userName | Out-File $tmpfile -Append
 
+# Download QlikSense Setup.exe here!!! (This sample uses Notepad++ as an example)
 Invoke-WebRequest 'https://notepad-plus-plus.org/repository/7.x/7.1/npp.7.1.Installer.x64.exe' -OutFile "$env:TEMP\npp.exe"
 
 & "$env:TEMP\npp.exe" /S
