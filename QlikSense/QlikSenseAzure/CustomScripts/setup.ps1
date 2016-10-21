0513daa442e5b3001c97aa0ee722f845aaf5ff83
Jeff Goldberg <goldbergjeffrey@yahoo.com>
2016-10-21 16:58:16 -0400

    another update to the script

--- QlikSense/QlikSenseAzure/CustomScripts/setup.ps1
+++ QlikSense/QlikSenseAzure/CustomScripts/setup.ps1
@@ -40,3 +40,29 @@ Invoke-WebRequest 'https://da3hntz84uekx.cloudfront.net/QlikSense/3.1.1/1/_MSI/Q
 & "c:\tmp\Qlik_Sense_setup.exe" -silent -log "c:\tmp\qliksenseinstall.log" userwithdomain="$combinedName" userpassword="$password" dbpassword="$password" hostname="$vmname"
 
 #& "c:\tmp\setup.exe" /S
+"Adding firewall rule to Windows Firewall to match Azure port configuration" | Out-File $tmpfile -Append
+New-NetFirewallRule -DisplayName "Qlik Sense" -Direction Inbound -action Allow -Protocol TCP -LocalPort 80,443,4248,4244 | Out-File $tmpfile -Append
+
+function Disable-InternetExplorerESC {
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+    Stop-Process -Name Explorer
+    "IE Enhanced Security Configuration (ESC) has been disabled." | Out-File $tmpfile -Append
+}
+function Enable-InternetExplorerESC {
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 1
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 1
+    Stop-Process -Name Explorer
+    Write-Host "IE Enhanced Security Configuration (ESC) has been enabled." -ForegroundColor Green
+}
+function Disable-UserAccessControl {
+    Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value 00000000
+    Write-Host "User Access Control (UAC) has been disabled." -ForegroundColor Green    
+}
+
+"Disabling Internet Explorer Security Mode" | Out-File $tmpfile -Append
+Disable-InternetExplorerESC
