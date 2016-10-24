56e1d763349e196f05412afa0db5e996958a3a66
Jeff Goldberg <goldbergjeffrey@yahoo.com>
2016-10-24 12:05:49 -0400

    another update to the script

--- QlikSense/QlikSenseAzure/CustomScripts/setup.ps1
+++ QlikSense/QlikSenseAzure/CustomScripts/setup.ps1
@@ -63,16 +63,11 @@ function timestamp {
 	return $b
 }
 
-$regPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
-$Name = "HelloWorld"
-$value = "powershell.exe -ExecutionPolicy Unrestricted -https://raw.githubusercontent.com/goldbergjeffrey/qlik/master/QlikSense/QlikSenseAzure/CustomScripts/hw.ps1"
-
-New-Item -Path $regPath -Force | Out-Null
-New-ItemProperty -Path $regPath -Name $Name -Value $value -PropertyType String -Force | Out-Null
-
 $regPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce"
 $Name = "HelloWorld"
-$value = "powershell.exe -ExecutionPolicy Unrestricted -https://raw.githubusercontent.com/goldbergjeffrey/qlik/master/QlikSense/QlikSenseAzure/CustomScripts/hw.ps1"
+$value = "powershell.exe -ExecutionPolicy Unrestricted `
+ -command (invoke-webrequest https://raw.githubusercontent.com/goldbergjeffrey/qlik/master/QlikSense/QlikSenseAzure/CustomScripts/hw.ps1 -outfile c:\tmp\hw.ps1 `
+ | powershell.exe -ExecutionPolicy Unrestricted -file c:\tmp\hw.ps1)"
 
 New-Item -Path $regPath -Force | Out-Null
 New-ItemProperty -Path $regPath -Name $Name -Value $value -PropertyType String -Force | Out-Null
