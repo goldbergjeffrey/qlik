5bf1afc77316220dcd8c9611c14c3195f936e49c
Jeff Goldberg <goldbergjeffrey@yahoo.com>
2016-10-24 10:33:48 -0400

    another update to the script

---
+++ QlikSense/QlikSenseAzure/CustomScripts/hw.ps1
@@ -0,0 +1,11 @@
+# If Path doesn't exist then create it
+if (-Not (Test-Path "c:\tmp"))
+{
+	mkdir "c:\tmp"
+}
+
+$tmpfile = "c:\tmp\hw.txt"
+
+$user = [Security.Principal.WindowsIdentity]::GetCurrent().Name
+
+"Hello " + $user | Out-File $tmpfile -Append
